83d5eac966ae186d21c573ad1f7bd0b677abcf52
Rodrigo Marins <143215565+Dev-Rodrigo-Marins@users.noreply.github.com>
2024-02-06 01:03:54 -0300

    Add files via upload
    codigo testado e com a implementação em python capturando um frame da camera

---
+++ arquivos/captura_frame.py
@@ -0,0 +1,23 @@
+import requests
+from PIL import Image
+from io import BytesIO
+
+# Link da imagem no servidor
+url = 'http://192.168.0.106/camera'
+
+# Faz uma requisição HTTP para obter a imagem
+response = requests.get(url)
+
+# Verifica se a requisição foi bem-sucedida (código de status 200)
+if response.status_code == 200:
+    # Lê os bytes da imagem
+    image_bytes = BytesIO(response.content)
+
+    # Abre a imagem usando a biblioteca PIL
+    image = Image.open(image_bytes)
+
+    # Salva a imagem no seu computador
+    image.save('/home/rodrigo/Arduino/codigo_correto_live_esp_ov76701/Imagens/imagem_salva.jpg') 
+    print('Imagem salva com sucesso!')
+else:
+    print(f'Erro na requisição HTTP. Código de status: {response.status_code}')
